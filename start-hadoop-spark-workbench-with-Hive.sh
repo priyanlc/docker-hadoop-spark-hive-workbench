6508727aa8f14a0fbfba70f817a6140107477a79
Priyan <priyan@localhost.localdomain>
2019-09-11 09:23:07 +0100

    regular ocmmit

--- start-hadoop-spark-workbench-with-Hive.sh
+++ start-hadoop-spark-workbench-with-Hive.sh
@@ -3,11 +3,12 @@
 docker-compose -f docker-compose.yml up -d namenode hive-metastore-postgresql
 docker-compose -f docker-compose.yml up -d datanode1 datanode2 hive-metastore
 docker-compose -f docker-compose.yml up -d hive-server
-docker-compose -f docker-compose.yml up -d spark-master spark-worker1 spark-worker2 spark-notebook hue
+docker-compose -f docker-compose.yml up -d spark-master spark-worker1 spark-worker2 zeppelin hue
 
 my_ip=`ip route get 1|awk '{print $NF;exit}'`
 echo "Namenode: http://${my_ip}:50070"
 echo "Datanode: http://${my_ip}:50075"
 echo "Spark-master: http://${my_ip}:8080"
 echo "Hue (HDFS Filebrowser): http://${my_ip}:8088/home"
+echo "zeppelin : http://${my_ip}:9090"
 
